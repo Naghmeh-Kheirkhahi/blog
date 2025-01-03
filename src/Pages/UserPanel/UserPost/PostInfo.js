fa58fc2642180860f7838b20c42c4524c8231196
naghmeh <melodycharitable4774@gmail.com>
2025-01-03 01:54:16 +0100

    finished the design of userpanel menu

--- src/Pages/UserPanel/UserPost/PostInfo.js
+++ src/Pages/UserPanel/UserPost/PostInfo.js
@@ -1,5 +1,7 @@
 
 import React from "react";
+import "./UserPost.css";
+import UserPanelNav from "../UserPanelNav";
 
 
 
@@ -7,7 +9,48 @@ function PostInfo() {
 
     return (
         <>
-            <h1>Post Information</h1>
+            <div className='main-page'>
+                <div className="row">
+                    <div className="col-3">
+                        <UserPanelNav />
+                    </div>
+
+
+                    <div className="col-9">
+                        <div className="main-container">
+                            <h1>My Post Information</h1>
+
+                            <p className="give-data">Please enter your post information!</p>
+
+                            <form className="postInfo-form">
+
+                                <label for="Image">Image</label>
+                                <input type="file" className="ps-0 "/>
+
+                                <label for="Author">Title</label>
+                                <input type="text" />
+
+                                <label for="Category">Category</label>
+                                <input type="text" />
+
+                                <label for="Title">Author</label>
+                                <input type="text" />
+
+                                <label for="Description">Description</label>
+                                <input type="text" />
+
+                                <label for="Date">Date</label>
+                                <input type="date" />
+
+                                <label for="ReadTime">Read Time</label>
+                                <input type="number" />
+
+                                <button type="submit" className="light-btn">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/Pages/UserPanel/UserPost/PostPreview.js
+++ src/Pages/UserPanel/UserPost/PostPreview.js
@@ -1,12 +1,50 @@
 
 import React from "react";
+import "./UserPost.css";
+import UserPanelNav from "../UserPanelNav";
+import BlogPost from "../../../Assets/Images/blogPost.png";
 
 
 function PostPreview() {
-    
+
     return (
         <>
-            <h1>Post Preview</h1>
+            <div className='main-page'>
+                <div className="row">
+                    <div className="col-3">
+                        <UserPanelNav />
+                    </div>
+
+
+                    <div className="col-9">
+                        <div className="main-container">
+                            <h1>My Preview Post</h1>
+
+                            <div className="post-preview">
+                                <div>
+                                    <img src={BlogPost} alt="blog post"/>
+                                </div>
+
+                                <div className="pt-4">
+                                    <div className="post-pre-info">
+                                        <p className="post-pre-category">Category</p>
+                                        <p className="post-pre-author"><span>By</span> Author</p>
+                                    </div>
+
+                                    <h3>Title</h3>
+
+                                    <p className="post-pre-description">Description</p>
+
+                                    <div className="post-pre-date">
+                                        <p><i class="fa fa-calendar"></i> Date</p>
+                                        <p><i class="fa fa-clock-o"></i> ? Min Read Time</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
