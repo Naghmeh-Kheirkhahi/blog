5d5a06904d20f9443f2a3fa0cb8b940bc48d9909
naghmeh <melodycharitable4774@gmail.com>
2025-01-06 13:41:57 +0100

    put an icon as a username in navbar to access the userpanel easily

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,9 +1,10 @@
 
-import React from 'react';
+import React, { useContext } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import websiteLogo from './../../Assets/Images/blogLogo.png';
 import { useState } from 'react';
+import { UserContext } from '../../Context/UserContext';
 
 
 
@@ -11,16 +12,17 @@ import { useState } from 'react';
 function Navbar() {
 
     const [query, setQuery] = useState('');
+    const { username } = useContext(UserContext);
 
 
     const handleSearch = () => {
-        const response= fetch(`/blog?search=${query}`)
+        const response = fetch(`/blog?search=${query}`)
         const data = response.json;
     }
 
 
     const handleKeyDown = (e) => {
-        if(e.key === 'Enter'){
+        if (e.key === 'Enter') {
             handleSearch();
         }
     }
@@ -32,7 +34,7 @@ function Navbar() {
             <nav class="navbar navbar-expand-sm position-fixed w-100 ps-2 pe-5">
                 <div class="container-fluid">
                     <Link class="navbar-brand navLogo" to="/"><img src={websiteLogo} alt='logo' /></Link>
-                    
+
                     <div class="collapse navbar-collapse" id="mynavbar">
                         <ul class="navbar-nav me-auto">
                             <li class="nav-item">
@@ -48,10 +50,15 @@ function Navbar() {
                                 <Link class='navLink' to="/Blog">Blog</Link>
                             </li>
                         </ul>
+
+                        {username ? <Link class='navLogin' to="/UserPanel"><i class="fa fa-user"></i> {username}</Link> : ''}
+
+
                         <form>
-                            <input type="text" placeholder="Search..." value={query} onChange={(e)=> setQuery(e.target.value)} onKeyDown={handleKeyDown}/>
+                            <input type="text" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} />
                             <button type="button" onClick={handleSearch}><i class="fa fa-search"></i></button>
                         </form>
+
                     </div>
                 </div>
             </nav>
