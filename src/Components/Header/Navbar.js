371514357fd10763b51404119cf47bef852fff14
naghmeh <melodycharitable4774@gmail.com>
2025-07-13 22:54:45 +0200

    made navbar menu responsive

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -27,38 +27,43 @@ function Navbar() {
         }
     }
 
-
+    // navbar navbar-expand-sm position-fixed w-100 ps-2 pe-5
+    // navLogo for img
 
     return (
         <>
-            <nav class="navbar navbar-expand-sm position-fixed w-100 ps-2 pe-5">
+            <nav class="navbar navbar-expand-lg position-fixed">
                 <div class="container-fluid">
-                    <Link class="navbar-brand navLogo" to="/"><img src={websiteLogo} alt='logo' /></Link>
+                    <Link className="navbar-brand" to="/">
+                        <img src={websiteLogo} alt="Logo" className="nav-logo" />
+                    </Link>
 
-                    <div class="collapse navbar-collapse" id="mynavbar">
-                        <ul class="navbar-nav me-auto">
-                            <li class="nav-item">
-                                <Link class='navLink' to="/">Home</Link>
+                    <button className="navbar-toggler me-3 custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="custom-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse ms-4" id="navbarNav">
+                        <ul className="navbar-nav me-auto mt-2 mt-xxl-3 mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class='navLink' to="/About">About Us</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/About">About Us</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class='navLink' to="/Contact">Contact Us</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Contact">Contact Us</Link>
                             </li>
                             <li class="nav-item">
                                 <Link class='navLink' to="/Blog">Blog</Link>
                             </li>
                         </ul>
 
-                        {username ? <Link class='navLogin' to="/UserPanel"><i class="fa fa-user"></i> {username}</Link> : ''}
-
+                        {username ? <Link class='navLogin' to="/UserPanel"><i class="fa fa-user"></i>{username}</Link> : ''}
 
                         <form>
-                            <input type="text" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} />
-                            <button type="button" onClick={handleSearch}><i class="fa fa-search"></i></button>
+                            <input type="text" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} className='formInput' />
+                            <button type="button" onClick={handleSearch} className='formBtn'><i class="fa fa-search"></i></button>
                         </form>
-
                     </div>
                 </div>
             </nav>
